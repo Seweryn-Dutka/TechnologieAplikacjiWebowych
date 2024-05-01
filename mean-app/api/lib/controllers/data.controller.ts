92b11795af5a32791cb3d9f85a07d3213ffc591a
Seweryn-Dutka <sewerynaleksdutka@gmail.com>
2024-05-01 18:04:32 +0200

    lab7

--- mean-app/api/lib/controllers/data.controller.ts
+++ mean-app/api/lib/controllers/data.controller.ts
@@ -1,71 +1,79 @@
 import { Request, Response, NextFunction, Router } from 'express';
 import Controller from '../interfaces/controller.interface';
+import { checkPostCount } from "../middlewares/checkPostCount.middleware";
+import DataService from '../modules/services/data.service'
 
 let testArr = [4, 5, 6, 3, 5, 3, 7, 5, 13, 5, 6, 4, 3, 6, 3, 6];
 
 class PostController implements Controller {
     public path = '/api/post';
     public router = Router();
+    public dataService = new DataService;
 
     constructor() {
         this.initializeRoutes();
     }
 
     private initializeRoutes() {
-        this.router.get(`${this.path}s`, this.getAll.bind(this));
-        this.router.post(`${this.path}/:id`, this.addData.bind(this));
-        this.router.get(`${this.path}/:id`, this.getPost.bind(this));
-        this.router.post(`${this.path}`, this.addPost.bind(this));
-        this.router.delete(`${this.path}/:id`, this.deletePost.bind(this));
-        this.router.post(`${this.path}/:num`, this.getNthPost.bind(this));
-        this.router.delete(`${this.path}s`, this.deleteAll.bind(this));
+        this.router.post(`${this.path}`, this.addPost);
+        this.router.post(`${this.path}/:num`, checkPostCount, this.getNumPosts);
+
+        this.router.get(`${this.path}/:id`, this.getPostById);
+        this.router.get(`${this.path}s`, this.getAllPosts);
+        
+        this.router.delete(`${this.path}:id`, this.deletePostById);
+        this.router.delete(`${this.path}s`, this.deleteAllPost);
     }
 
-    private getAll(response: Response){        
-        response.status(200).json(testArr);
+    // POST
+    private addPost = async (request: Request, response: Response, next: NextFunction) => {
+        const {title, text, image} = request.body;
+        const readingData = {
+            title,
+            text,
+            image
+        };
+        try {
+            await this.dataService.addPost(readingData);
+            response.status(200).json(readingData);
+        } catch (error) {
+                console.log('eeee', error)
+                console.error(`Validation Error: ${error.message}`);
+                response.status(400).json({error: 'Invalid input data.'});
+        }
+    }
+
+    private getNumPosts = async (request: Request, response: Response, next: NextFunction) => {
+        const { num } = request.params;
+        const numData = await this.dataService.getNumPosts(num);
+        response.status(200).json(numData);
+    };    
+
+    // GET
+    private getPostById = async (request: Request, response: Response, next: NextFunction) => {
+        const { id } = request.params;
+        const post = await this.dataService.getPostById({ _id: id });
+        response.status(200).json(post);
     }
     
-    private addData(request: Request, response: Response) {
-        testArr.push(request.body.data);
-        response.status(200).json(testArr);
+    private getAllPosts = async (request: Request, response: Response, next: NextFunction) => {
+        const posts = await this.dataService.getAllPosts();
+        response.status(200).json(posts);
     };
 
+    // DELETE
+    private deletePostById = async (request: Request, response: Response, next: NextFunction) => {
+        const { id } = request.params;
+        await this.dataService.deletePostById({_id: id});
+        response.status(200);
+    };
 
-    private getPost(request: Request, response: Response) {
-        const id = Number(request.params.id);
-        if (!Number.isInteger(id) || id >= testArr.length || id < 0) {
-            response.status(400).json({ exception: 'Invalid ID' });
-            return;
-        }
-        response.status(200).json(testArr[id]);
-    }
-
-    private addPost(request: Request, response: Response) {
-        testArr.push(request.body.data);
-        response.status(201).send();
-    }
-
-    private deletePost(request: Request, response: Response) {
-        const id = Number(request.params.id);
-        if (!Number.isInteger(id) || id >= testArr.length || id < 0) {
-            response.status(400).json({ exception: 'Invalid ID' });
-            return;
-        }
-        testArr.splice(id, 1);
-        response.status(204).send();
-    }
-
-    private getNthPost(request: Request, response: Response) {
-        const num = Number(request.params.num);
-        const posts = testArr.slice(0, num);
-        response.status(200).json(posts);
-    }
-
-    private deleteAll(request: Request, response: Response) {
-        testArr = [];
-        response.status(200).json(testArr);
-    }
-
+    private deleteAllPost = async (request: Request, response: Response, next: NextFunction) => {
+        const { id } = request.params;
+        await this.dataService.deleteAllPosts();
+        response.status(200);
+    };
+    
 }
 
 export default PostController;
