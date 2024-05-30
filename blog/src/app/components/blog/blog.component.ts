611728a738560fac494aa9b735d3e3b2b72987e0
Seweryn-Dutka <sewerynaleksdutka@gmail.com>
2024-05-30 21:30:52 +0200

    fixes

--- blog/src/app/components/blog/blog.component.ts
+++ blog/src/app/components/blog/blog.component.ts
@@ -1,24 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import {DataService} from "../../services/data.service";
-import {BlogItemComponent} from "../blog-item/blog-item.component";
-import {CommonModule} from "@angular/common";
+import { Component, Input, OnInit } from '@angular/core';
+import { DataService } from "../../services/data.service";
+import { BlogItemComponent } from "../blog-item/blog-item.component";
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { FilterTextPipe } from '../../pipes/filter-text.pipe';
+import { SearchBarComponent } from '../../shared/search-bar/search-bar.component';
 
 @Component({
- selector: 'blog',
- standalone: true,
- imports: [BlogItemComponent, CommonModule],
- providers: [DataService],
- templateUrl: './blog.component.html',
- styleUrl: './blog.component.css'
+  selector: 'blog',
+  standalone: true,
+  imports: [HttpClientModule, BlogItemComponent, CommonModule, SearchBarComponent, FilterTextPipe],
+  providers: [DataService],
+  templateUrl: './blog.component.html',
+  styleUrl: './blog.component.css'
 })
-export class BlogComponent implements OnInit{
- public items: any;
+export class BlogComponent implements OnInit {
+  public items$: any;
+  @Input() filterText: string = '';
 
- constructor(private service: DataService) {
- }
+  constructor(private service: DataService) {
+  }
+  ngOnInit() {
+    this.getAll();
+  }
 
- ngOnInit() {
-   this.items = this.service.getAll();
- }
-
+  getAll() {
+    this.service.getAll().subscribe(response => {
+      this.items$ = response;
+    });
+  }
 }
